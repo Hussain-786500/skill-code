640654210ff01bf090e6cf7c6e7d67877f9acebc
Ibrahim Hussain <ibrahimhussain0928@gmail.com>
2025-09-29 09:22:30 +0530

    Scop & Global

---
+++ Class8.js
@@ -0,0 +1,47 @@
+// ******************** scope & global ********************
+
+// let a = 300
+
+// if (true){
+//     let a = 10
+//     const b = 20
+//     console.log("inner", a);
+// }
+// console.log(a)
+
+
+// function one(){
+//     const username = "ibrahim"
+
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username)
+//     }
+
+//      two()
+
+// }
+// one()
+
+// if (true) {
+//     const username = "hussain"
+//     if (username === "hussain") {
+//         const website = " fecebook"
+//         console.log(username + website);
+//     }
+//     // console.log(website);
+// }
+// console.log(username)
+
+// ********************* important *****************
+// console.log(addone(5))
+// function addone(num) {
+//     return (num + 1)
+// }
+
+// const addTwo = function(num){
+//     return ( num + 1 )
+// }
+// console.log(addTwo(5))
+
