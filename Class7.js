0c2476544e8335d8c3f81295dc480238b9bf1d64
Ibrahim Hussain <ibrahimhussain0928@gmail.com>
2025-09-29 09:21:38 +0530

    Function Concern

---
+++ Class7.js
@@ -0,0 +1,71 @@
+// **************************** FUNCTION ***************************
+
+// function myName (){
+//     console.log("I")
+//     console.log("B")
+//     console.log("R")
+//     console.log("A")
+//     console.log("H")
+//     console.log("I")
+//     console.log("M")
+// }
+// myName()
+
+// function addNumbers(number1, number2){
+//     return (number1 + number2) 
+                        
+// }
+// const result = addNumbers(5, 5);
+
+// console.log("result:", result);
+
+
+// function userMessage(username){
+    
+//     return `${username} just logged in`
+// }
+// console.log(userMessage("ibrahim")) //ibrahim just logged in
+
+
+
+
+// function userMessage(username){
+//     if(!username){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(userMessage())        //Please enter a username
+//                                                   undefined
+
+      
+
+// function calculateCarPrice(val1, val2,...num1){
+//     return num1
+// }
+
+// console.log(calculateCarPrice(100, 10, 30, 4000, 40, ))  //[ 4000, 40 ], val1 and val2 rest
+
+
+// object pass in function................
+
+// const user = {
+//     username: "ibrahim",
+//     price: 199
+// }
+
+// function userObject(anyobject){
+//   console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
+
+// userObject(user)
+
+// array pass in function.....................
+
+// const myNewArr = [12, 34, 56, 20]
+
+// function returnValue (getArray){
+//     return (getArray[1])
+// }
+// console.log(returnValue(myNewArr))   //34
