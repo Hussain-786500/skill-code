ef67e367726453220982d7495107a91cef130613
Ibrahim Hussain <ibrahimhussain0928@gmail.com>
2025-10-02 09:23:27 +0530

    execute code_ &_ call stack

---
+++ Class11.js
@@ -0,0 +1,20 @@
+// ******** execute code + cal stack *******
+
+// * javaScript Execution Context...
+
+// 1. Global Execution Context
+// 2. Function Execution Context
+// 3. Eval Execution Context
+
+
+// let val1 = 20
+// let val2 = 10
+// function addNum(num1, num2){
+//     let total = num1 + num2
+//     return total;
+// }
+
+// let result1 = addNum(val1, val2)
+// let result2 = addNum(20, 10)
+
+// console.log(val1 + val2)     //30
