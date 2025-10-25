b78e3ca021174d1eecd1ab8ab54bfbc694ec2525
Ibrahim Hussain <ibrahimhussain0928@gmail.com>
2025-10-25 16:01:46 +0530

    Arrow Function

---
+++ Class9.js
@@ -0,0 +1,51 @@
+// ****************** Arro functin **************
+
+// const user = {
+//     username: "ibrahim",
+//     price: 999,
+//     welcomeMessage: function() {
+//         console.log(`${this.username}, welcome to google.AI ${this.price} Entry price`);
+//     //    console.log(this)
+//     }
+// }
+
+// // user.welcomeMessage()
+// // user.username = "rahul"
+// // user.price = 888
+// // user.welcomeMessage()
+// console.log(this)
+
+
+// function code(){
+//     console.log(this);
+// }
+// code()
+
+// const tag = function () {
+//     let username = "ibrahim"
+//     console.log(this.username)
+// }
+
+// tag()                      // undefined
+
+// const tag = () => {
+//     let username = "ibrahim"
+//     console.log(this)
+// }
+
+// tag()             //undefined
+
+
+
+//normal arro function............
+// const addone = (num1, num2) => {   //if you use {} then put return value.
+//     return num1 + num2
+// }
+
+// console.log(addone(5,5))
+
+//imp-lesit return arro function.............
+
+// const addone = (num1, num2) =>  (num1 + num2)  // if you use () then do not put return value.
+
+// console.log(addone(5,5))
